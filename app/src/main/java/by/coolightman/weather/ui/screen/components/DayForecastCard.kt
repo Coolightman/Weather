959443cc665ba99a7051ac4ffa4ce0fd0d476e7c
coolightman <drugak48@gmail.com>
2022-11-21 10:27:59 +0300

    update day forecast icons

--- app/src/main/java/by/coolightman/weather/ui/screen/components/DayForecastCard.kt
+++ app/src/main/java/by/coolightman/weather/ui/screen/components/DayForecastCard.kt
@@ -1,6 +1,5 @@
 package by.coolightman.weather.ui.screen.components
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
